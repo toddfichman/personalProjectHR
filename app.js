eb122a94ccef812e986d3e30e03dad5dc5e5268e
Todd Fichman <toddfichman@gmail.com>
2018-06-01 13:11:34 -0400

    Added the abbility to store the data that is typed into the text area

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 	$('.submitForm').on('click', function(){
 		let textFieldValue = $('.textField').val();
-		console.log(textFieldValue);
 		$('.debug').text(textFieldValue);
-	})
+
+		localStorage.setItem('myFormTextData', textFieldValue);
+	});
+
+	// $('.textField').on('keyup', function(){
+	// 	let textFieldValue = $('.textField').val();
+	// 	$('.debug').text(textFieldValue);
+	// });
 });
