2db6375732903b112943be7123170cbf022002d7
Todd Fichman <toddfichman@gmail.com>
2018-06-01 19:29:26 -0400

    Added a display all buttom that displays the values in local storage

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
 $(document).ready(function(){
-	$('.submitForm').on('click', function(){
-		let textFieldValue = $('.textField').val();
-		$('.debug').text(textFieldValue);
+	let dataStorage = [];
+	
+	
+	$('.setData').on('click', function(){
+		
+		let data = {};
+		let textFieldValue = $('.textField').val(); //key 
+		let textAreaValue = $('.textArea').val();  //value
+		$('.display').text(textAreaValue);
 
-		localStorage.setItem('myFormTextData', textFieldValue);
+		function storeData(obj) {
+			obj[textFieldValue] = textAreaValue;
+			if(textFieldValue === ''){
+				alert ("Please fill in snippet name");
+			} else if (textFieldValue !== ''){
+				dataStorage.push(obj);
+				localStorage.setItem("item", JSON.stringify(dataStorage));
+			}
+			
+		}
+		storeData(data);
+		// localStorage.setItem('myFormTextData', textFieldValue);
+		// localStorage.setItem('textAreaData', textAreaValue);
+		$('.textField').val('');
+		$('.textArea').val('');
 	});
 
-	// $('.textField').on('keyup', function(){
-	// 	let textFieldValue = $('.textField').val();
-	// 	$('.debug').text(textFieldValue);
-	// });
+	$('.getData').on('click', function(){
+		let retrievedData = localStorage.getItem('textAreaData');
+		$('.display').text(retrievedData);
+	})
+
+	$('.displayAll').on('click', function(){
+		// let f = JSON.parse('item');
+		console.log('item');
+		let retrievedData = localStorage.getItem('item');
+		$('.display').text(retrievedData);
+		// for (let i = 0; i < dataStorage.length; i++){
+		// 	for (let key in dataStorage[i]){
+		// 		let value = dataStorage[i][key];
+		// 		// $('.display').text(value);
+		// 		console.log(value);
+		// 	}
+		// }		
+	});
+
 });
