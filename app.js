83caec47387eeb609ab363658a77ee66a4710467
Todd Fichman <toddfichman@gmail.com>
2018-06-05 16:11:48 -0400

    Used HTML and basic CSS to give page some structure and still working on JS to display propery data

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
 		let data = {};
 		let textFieldValue = $('.textField').val(); //key 
 		let textAreaValue = $('.textArea').val();  //value
-		$('.display').text(textAreaValue);
 
 		function storeData(obj) {
 			obj[textFieldValue] = textAreaValue;
@@ -16,33 +15,48 @@ $(document).ready(function(){
 			} else if (textFieldValue !== ''){
 				dataStorage.push(obj);
 				localStorage.setItem("item", JSON.stringify(dataStorage));
+				$('.displayRecent').text('Most Recent snippet --> ' + textFieldValue + ': ' + textAreaValue);
 			}
-			
 		}
 		storeData(data);
-		// localStorage.setItem('myFormTextData', textFieldValue);
-		// localStorage.setItem('textAreaData', textAreaValue);
 		$('.textField').val('');
 		$('.textArea').val('');
 	});
 
-	$('.getData').on('click', function(){
-		let retrievedData = localStorage.getItem('textAreaData');
-		$('.display').text(retrievedData);
+	$('.searchData').on('click', function(){
+		let searchInput = $('.searchField').val();
+		if (searchInput.length === 0){
+			alert('Please a snippet name to search for')
+		}
+		let retrievedData = localStorage.getItem("item");
+		let nameAndSnippet = JSON.parse(retrievedData);
+		for (let i = 0; i < nameAndSnippet.length; i++){
+			let nameOfSnippet = Object.keys(nameAndSnippet[i]) // this === textFieldValue
+			// console.log(nameOfSnippet);
+			if(nameOfSnippet.includes(searchInput)){ //use includes for search of keys
+				for (var key in nameAndSnippet[i]){
+					$('.displaySearch').text('Snippet of last search: ' + nameAndSnippet[i][key]);
+				}
+			} 
+			// else if (!(nameOfSnippet.includes(searchInput))) {
+			// 	alert("The snippet name you entered does not exist");
+			// }
+		}
 	})
 
 	$('.displayAll').on('click', function(){
-		// let f = JSON.parse('item');
-		console.log('item');
-		let retrievedData = localStorage.getItem('item');
-		$('.display').text(retrievedData);
-		// for (let i = 0; i < dataStorage.length; i++){
-		// 	for (let key in dataStorage[i]){
-		// 		let value = dataStorage[i][key];
-		// 		// $('.display').text(value);
-		// 		console.log(value);
-		// 	}
-		// }		
+		let retrievedData = localStorage.getItem("item");
+		let nameAndSnippet = JSON.parse(retrievedData);
+		for (let i = 0; i < nameAndSnippet.length; i++){
+			for (var key in nameAndSnippet[i]){
+				let snippet = nameAndSnippet[i][key]
+				$('.displayAllArea').text(snippet);
+				console.log(snippet) // this === textAreaValue
+			}
+			
+		}
+		
+		$('.displayAllArea').text(retrievedData);		
 	});
 
 });
